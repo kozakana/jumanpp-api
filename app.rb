b68f7f038f9d702c14b37e0ab6fe648c69c25782
mori-goki <mori-goki@kayac.com>
2018-05-06 22:53:11 +0900

    add error handling

--- app.rb
+++ app.rb
@@ -12,32 +12,70 @@ PARSE_ELEMTS = [
 config = YAML.load_file('./config.yml')
 juman = JumanppRuby::Juman.new(**config)
 
+def string_param
+  if request['HTTP_ACCEPT'] == 'application/json'
+    req = request.body.read
+    JSON.parse(req)['string']
+  else
+    params[:string]
+  end
+end
+
 post '/split', provides: :json do
   content_type :json
+  data = {}
 
-  str = params[:string]
-
-  words = juman.parse(str)
-  if words.last == 'EOS'
-    words = words.slice(0...-1)
+  begin
+    str = string_param
+    raise unless str
+  rescue
+    data[:status] = 'fail'
+    status 400
+    return data.to_json
   end
 
-  data = {}
+  begin
+    words = juman.parse(str)
+    if words.last == 'EOS'
+      words = words.slice(0...-1)
+    end
+  rescue
+    data[:status] = 'fail'
+    status 500
+    return data.to_json
+  end
+
   data[:status] = 'success'
   data[:results] = words
   data.to_json
 end
 
 post '/parse', provides: :json do
-  str = params[:string]
+  content_type :json
+  data = {}
 
-  words = []
-  juman.parse(str) do |word|
-    words << word
+  begin
+    str = string_param
+    raise unless str
+  rescue
+    data[:status] = 'fail'
+    status 400
+    return data.to_json
   end
 
-  if words.last == ['EOS']
-    words = words.slice(0...-1)
+  begin
+    words = []
+    juman.parse(str) do |word|
+      words << word
+    end
+
+    if words.last == ['EOS']
+      words = words.slice(0...-1)
+    end
+  rescue
+    data[:status] = 'fail'
+    status 500
+    return data.to_json
   end
 
   data = {}
